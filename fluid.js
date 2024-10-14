0ed2c1c306068f16eddeb727989e18bb24c2974d
tamilkumaran <tamilkumaran1494@gmail.com>
2024-10-14 14:14:00 +0530

    fluid animation added

---
+++ fluid.js
@@ -0,0 +1,68 @@
+const canvas = document.getElementById('fluidCanvas');
+const ctx = canvas.getContext('2d');
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+resizeCanvas();
+window.addEventListener('resize', resizeCanvas);
+
+const particles = [];
+const numParticles = 100; 
+
+class Particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        
+        this.vx = Math.random() * 2 - 1;
+        this.vy = Math.random() * 2 - 1;
+
+        this.radius = Math.random() * 5 + 2;
+
+        this.color = `rgb(38, 38, 38)`;
+    }
+
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
+
+        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+            this.vx = -this.vx;
+        }
+        if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+            this.vy = -this.vy;
+        }
+
+        this.draw();
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+function initParticles() {
+    for (let i = 0; i < numParticles; i++) {
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height;
+        particles.push(new Particle(x, y));
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    particles.forEach(particle => particle.update());
+
+    requestAnimationFrame(animate);
+}
+
+initParticles();
+animate();
